c228ab3bfc85469a454a4181603ed3c6e5fdf659
Hebe-s <2940500426@qq.com>
2020-02-28 19:15:47 +0800

    123456

---
+++ src/main/java/com/example/ok/CostBean.java
@@ -0,0 +1,9 @@
+package com.example.ok;
+
+import java.io.Serializable;
+
+public class CostBean implements Serializable {
+    public String costTitle;
+    public String costDate;
+    public String costMoney;
+}
